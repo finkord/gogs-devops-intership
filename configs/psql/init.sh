a382dc08ee6c290a8b465b4b478ddf64aeb93e9c
finkord <devfinkord@gmail.com>
2025-07-02 11:03:20 +0300

    Rename jenkins.yml to jenkins-compose.yml; attach Jenkins container to gogs_gogs_network; connect SonarQube with PostgreSQL database; update init.sh configuration for database creation, adding SonarQube user and database creation.

--- configs/psql/init.sh
+++ configs/psql/init.sh
@@ -2,34 +2,27 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-    DO
-    \$do\$
-    BEGIN
-       IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = '$POSTGRES_GOGS_USER') THEN
-           CREATE USER $POSTGRES_GOGS_USER WITH PASSWORD '$POSTGRES_GOGS_PASSWORD';
-       END IF;
-    END
-    \$do\$;
+    -- Create Gogs user with password
+    CREATE USER $POSTGRES_GOGS_USER WITH PASSWORD '$POSTGRES_GOGS_PASSWORD';
 
-    DO
-    \$do\$
-    BEGIN
-       IF NOT EXISTS (SELECT FROM pg_database WHERE datname = '$POSTGRES_GOGS_DB') THEN
-           CREATE DATABASE $POSTGRES_GOGS_DB OWNER $POSTGRES_GOGS_USER;
-       END IF;
-    END
-    \$do\$;
+    -- Create Gogs database owned by Gogs user
+    CREATE DATABASE $POSTGRES_GOGS_DB OWNER $POSTGRES_GOGS_USER;
 
+    -- Grant all privileges on Gogs database to Gogs user
     GRANT ALL PRIVILEGES ON DATABASE $POSTGRES_GOGS_DB TO $POSTGRES_GOGS_USER;
 
-    DO
-    \$do\$
-    BEGIN
-       IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = '$POSTGRES_OTEL_USER') THEN
-           CREATE USER $POSTGRES_OTEL_USER WITH PASSWORD '$POSTGRES_OTEL_PASSWORD';
-       END IF;
-    END
-    \$do\$;
+    -- Create OpenTelemetry user with password
+    CREATE USER $POSTGRES_OTEL_USER WITH PASSWORD '$POSTGRES_OTEL_PASSWORD';
 
+    -- Grant pg_monitor role to OpenTelemetry user for monitoring permissions
     GRANT pg_monitor TO $POSTGRES_OTEL_USER;
+
+    -- Create SonarQube user with password
+    CREATE USER $SONARQUBE_SONAR_USER WITH PASSWORD '$SONARQUBE_SONAR_PASSWORD';
+
+    -- Create SonarQube database owned by SonarQube user
+    CREATE DATABASE $SONARQUBE_SONAR_DB OWNER $SONARQUBE_SONAR_USER;
+
+    -- Grant all privileges on SonarQube database to SonarQube user
+    GRANT ALL PRIVILEGES ON DATABASE $SONARQUBE_SONAR_DB TO $SONARQUBE_SONAR_USER;
 EOSQL
