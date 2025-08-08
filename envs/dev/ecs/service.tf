cfc992fc080ad493a547144fbc9cea06f5e83dcd
finkord <devfinkord@gmail.com>
2025-08-09 02:38:02 +0300

    modify autoscaling in ecs service

--- envs/dev/ecs/service.tf
+++ envs/dev/ecs/service.tf
@@ -19,7 +19,7 @@ resource "aws_ecs_service" "gogs" {
 }
 
 resource "aws_appautoscaling_target" "gogs" {
-  max_capacity       = 2
+  max_capacity       = 3
   min_capacity       = 1
   resource_id        = "service/${aws_ecs_cluster.gogs.name}/${aws_ecs_service.gogs.name}"
   scalable_dimension = "ecs:service:DesiredCount"
@@ -34,7 +34,7 @@ resource "aws_appautoscaling_policy" "gogs_cpu" {
   service_namespace  = aws_appautoscaling_target.gogs.service_namespace
 
   target_tracking_scaling_policy_configuration {
-    target_value = 70.0
+    target_value = 60.0
     predefined_metric_specification {
       predefined_metric_type = "ECSServiceAverageCPUUtilization"
     }
@@ -42,3 +42,21 @@ resource "aws_appautoscaling_policy" "gogs_cpu" {
     scale_out_cooldown = 60
   }
 }
+
+resource "aws_appautoscaling_policy" "gogs_memory" {
+  name               = "memory-scaling"
+  policy_type        = "TargetTrackingScaling"
+  resource_id        = aws_appautoscaling_target.gogs.resource_id
+  scalable_dimension = aws_appautoscaling_target.gogs.scalable_dimension
+  service_namespace  = aws_appautoscaling_target.gogs.service_namespace
+
+  target_tracking_scaling_policy_configuration {
+    target_value = 70.0
+    predefined_metric_specification {
+      predefined_metric_type = "ECSServiceAverageMemoryUtilization"
+    }
+    scale_in_cooldown  = 60
+    scale_out_cooldown = 60
+  }
+}
+
--- envs/dev/vpc/main.tf
+++ envs/dev/vpc/main.tf
@@ -8,7 +8,7 @@ module "vpc" {
   public_subnets  = ["10.1.0.0/24", "10.1.1.0/24"]
   private_subnets = ["10.1.2.0/24", "10.1.3.0/24"]
 
-  enable_nat_gateway     = true
+  enable_nat_gateway     = false
   single_nat_gateway     = true
   one_nat_gateway_per_az = false
   enable_vpn_gateway     = false
